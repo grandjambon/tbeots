1706588f38c35c55fcc427f122dea3e85665c456
grandjambon <phil@laurenlaverne.co.uk>
2018-02-27 13:31:52 +0000

    buffering the data so it only actually requests it once a minute

---
+++ src/main/java/com/pj/tbeots/data/BufferedDataManager.java
@@ -0,0 +1,64 @@
+package com.pj.tbeots.data;
+
+import com.pj.tbeots.data.json.JsonLeaguePosition;
+import com.pj.tbeots.data.json.JsonTeam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Quick and dirty attempt at reducing our load on the feed
+ */
+public class BufferedDataManager implements DataManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(BufferedDataManager.class);
+
+    private static final DateTimeFormatter tokenFormatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmm");
+    
+    private final DataManager dataManager;
+
+    private String cacheToken;
+    private Map<String, JsonTeam> teams;
+    private List<JsonLeaguePosition> leaguePositions;
+    private Map<String, List<String>> fixtures;
+
+    public BufferedDataManager(DataManager dataManager) {
+        this.dataManager = dataManager;
+    }
+
+    public synchronized void refreshCache(String cacheToken) throws IOException {
+        if (this.cacheToken == null || !this.cacheToken.equals(cacheToken)) {
+            logger.info("token received = {} - our token = {} so rebuilding cache", cacheToken, this.cacheToken);
+            this.teams = dataManager.getTeams();
+            this.leaguePositions = dataManager.getLeaguePositions();
+            this.fixtures = dataManager.getFixtures();
+            this.cacheToken = cacheToken;
+        }
+    }
+
+    @Override
+    public synchronized Map<String, JsonTeam> getTeams() throws IOException {
+        return teams;
+    }
+
+    @Override
+    public synchronized List<JsonLeaguePosition> getLeaguePositions() throws IOException {
+        return leaguePositions;
+    }
+
+    @Override
+    public synchronized Map<String, List<String>> getFixtures() throws IOException {
+        return fixtures;
+    }
+
+    public static String getLiveToken() {
+        return LocalDateTime.now(ZoneId.of("UTC")).format(tokenFormatter);
+    }
+}
--- src/main/java/com/pj/tbeots/springboot/TbeotsController.java
+++ src/main/java/com/pj/tbeots/springboot/TbeotsController.java
@@ -1,7 +1,10 @@
 package com.pj.tbeots.springboot;
 
 import com.pj.tbeots.data.DataManager;
+import com.pj.tbeots.data.FootballFeedsDataManager;
 import com.pj.tbeots.data.json.JsonLeaguePosition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -10,16 +13,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.io.IOException;
 import java.util.List;
 
+import static com.pj.tbeots.data.BufferedDataManager.getLiveToken;
+
 @SuppressWarnings("unused")
 @Controller
 public class TbeotsController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TbeotsController.class);
 
     @Autowired
     private DataManager dataManager;
 
     @RequestMapping("/")
     public String home(ModelMap model) throws IOException {
-        List<JsonLeaguePosition> leaguePositions = dataManager.getLeagueTable();
+        String cacheToken = getLiveToken();
+
+        logger.info("requesting with cacheToken = {} ", cacheToken);
+
+        dataManager.refreshCache(cacheToken);
+        List<JsonLeaguePosition> leaguePositions = dataManager.getLeaguePositions();
         model.addAttribute("leaguePositions", leaguePositions);
         model.addAttribute("fixtures", dataManager.getFixtures());
         return "home";

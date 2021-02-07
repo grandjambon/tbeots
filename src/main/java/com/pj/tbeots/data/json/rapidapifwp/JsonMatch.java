dc64d98d09f9e6af2abab7280565d74db338b56e
Phil Johnston <phil@laurenlaverne.co.uk>
2021-02-07 14:04:40 +0000

    ignore unknown properties

--- src/main/java/com/pj/tbeots/data/json/rapidapifwp/JsonMatch.java
+++ src/main/java/com/pj/tbeots/data/json/rapidapifwp/JsonMatch.java
@@ -1,5 +1,6 @@
 package com.pj.tbeots.data.json.rapidapifwp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSetter;
 
@@ -8,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Map;
 
 // from fixture-results in Fixtures/Results
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JsonMatch {
 
     public enum Status { RESULT, LIVE, FIXTURE;}
